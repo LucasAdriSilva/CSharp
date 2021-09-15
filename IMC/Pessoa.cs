81092f290796fc7f6f790e6ae63c8498e57bf47d
Lucas Adriano <Lucasadrisilva@hotmail.com>
2021-09-14 21:53:23 -0300

    feat(CSharp): Teste de calcculadora IMC

---
+++ IMC/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    class Pessoa
+    {
+        public Pessoa() {
+            this.Nome = "";
+            this.peso = 0.;
+            this.altura = 0.;
+            this.idade = 0;
+        }
+        private int idade;
+
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        private String nome;
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }        
+
+        public float peso { get; set; }
+        public float altura { get; set; }
+        
+        public void ExibirDados() {
+            Console.WriteLine("Olá "+this.Nome+" vc tem "+this.idade+ "  anos e seu Imc é: "+this.calcularImc(this.peso,this.altura));
+            Console.ReadKey();
+        }
+
+        public float calcularImc(float peso, float altura) {
+            var imc = peso / (altura*altura) ; 
+            return imc;
+        }
+    }
+}
---
+++ IMC/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+            Console.WriteLine("Calculando IMC");
+            Console.WriteLine("-----------------------");
+
+            Console.Write("Digite seu nome: ");
+            p.Nome = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua idade: ");
+            p.Idade = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Digite o seu Peso: ");
+            p.peso = Convert.ToInt32(Console.ReadLine());
+            Convert.ToDouble(p.peso); 
+            
+            Console.Write("Digite o sua Atura: ");
+            p.altura = Convert.ToInt32(Console.ReadLine());
+            Convert.ToDouble(p.altura);
+
+            p.ExibirDados();
+            
+
+
+
+        }
+    }
+}
