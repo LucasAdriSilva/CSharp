a301c2468ef136c2fec1370b78ea31f1fe2a01c7
Lucas Adriano <Lucasadrisilva@hotmail.com>
2021-09-15 15:39:02 -0300

    feat(CSharp): Criando Calculadora de Área de um Retangulo Usando Int

---
+++ AreaRetangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRetangulo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Retangulo ret = new Retangulo();
+            Console.WriteLine("Calcula a area de um RETANGULO");
+
+            Console.Write("Informe a Altura: ");
+            ret.Altura = Convert.ToInt32(Console.ReadLine());            
+
+            Console.Write("Informe a Base: ");
+            ret.Base = Convert.ToInt32(Console.ReadLine());
+             
+
+            ret.ExibeDados();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AreaRetangulo/Retangulo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace AreaRetangulo
+{
+    public class Retangulo
+    {
+        public Retangulo()
+        {
+            this.Base = 0;
+            this.Altura = 0;
+        }
+
+        //Base do Retângulo
+        private int baseret;
+        public int Base
+        {
+            get { return baseret; }
+            set
+            {
+                if (value >= 0) { baseret = value; }
+                else baseret = 0;
+
+            }
+        }
+
+
+        //Altura do Retângulo
+        private int alturadoret;
+
+        public int Altura
+        {
+            get { return alturadoret; }
+            set
+            {
+                if (value >= 0) { alturadoret = value; }
+                else alturadoret = 0;
+            }
+        }
+
+        public int Area
+        {
+            get { return alturadoret * baseret; }
+        }
+
+        public void ExibeDados()
+        {
+            Console.WriteLine("-------------------^------------------");
+            Console.WriteLine("Base: " + this.Base);
+            Console.WriteLine("Altura: " + this.Altura);
+            Console.WriteLine("Area: " + this.Area + "m²");
+
+
+        }
+
+    }
+}
