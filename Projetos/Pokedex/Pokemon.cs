c9a39cd859f86c22ebc703d3f778f5be90df10db
Lucas Adriano <Lucasadrisilva@hotmail.com>
2021-09-20 14:44:14 -0300

    feat(CSharp): Adição de projetos no git/ Batalha Pokemon

---
+++ Projetos/Pokedex/Pokemon.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex
+{
+    public class Pokemon
+    {   //Sobrecarga é crirar metodos que possuem o memso nome mas possuem parametros difrentescada um tendo um objetivo
+        public Pokemon()
+        {
+            this.Nome = "";
+            this.Descricao = "";
+        }
+
+        public Pokemon(String nome, String descricao)
+        {
+            this.Nome = nome;
+            this.Descricao = descricao;
+        }
+
+        private String nome;
+        public String Nome
+        {
+            get { return this.nome; }
+            set {
+                String texto = value.ToUpper();
+                this.nome = value; 
+            }
+        }
+
+
+        private String descricao;// aramazena o valor da propriedade descrição
+        public String Descricao // Representa a caracteristica Descrição do meu Pokemon
+        {
+            get { return this.descricao; }
+            set { 
+                this.descricao = value.ToUpper(); 
+            }
+        }
+
+        public void ExibirDadosPokemon(){
+            Console.WriteLine("Nome do Pokémon: "+ this.Nome);
+            Console.WriteLine("Descrição do Pokémon: " + this.Descricao);
+        }
+
+        public void ExibirDadosPokemon(Boolean formatado)
+        {
+            if (formatado == true)
+            {
+                Console.WriteLine("Pokémon "+ this.Nome + ": " + this.Descricao.ToLower());
+            }
+            else
+            {
+                Console.WriteLine("Nome do Pokémon: " + this.Nome);
+                Console.WriteLine("Descrição do Pokémon: " + this.Descricao);
+            }
+        }
+
+    }
+}
---
+++ Projetos/Pokedex/PokemonPlus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex
+{
+    public class PokemonPlus : Pokemon
+    {
+        public PokemonPlus():base()
+        {
+            this.Poder = 0;   
+        }
+
+        public PokemonPlus(String nome, String descricao, int poder) : base(nome, descricao)
+        {
+            this.Poder = poder;
+        }
+        public int Poder  {get; set;}
+
+        public void ExibirDadosPokemonPlus()
+        {
+            Console.WriteLine("Nome do Pokémon: " + this.Nome);
+            Console.WriteLine("Descrição do Pokémon: " + this.Descricao);
+            Console.WriteLine("Poder : " + this.Poder);
+        }
+    }
+}
