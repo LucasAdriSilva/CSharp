79eab4c283d6538ad4fd967c43b9a64c8abdb17b
Lucas Adriano <Lucasadrisilva@hotmail.com>
2021-09-27 09:03:47 -0300

    feat(CSharp): Trabalhando co destruidores

---
+++ Projetos/PooDestruidores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooDestruidores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ferramenta f1 = new Ferramenta("Alicate");
+            Ferramenta f2 = new Ferramenta("Tesoura");
+
+            f1 = null;
+            f2 = new Ferramenta("Furadeira");
+            GC.Collect();
+            Console.ReadKey();
+                
+
+        }
+    }
+}
