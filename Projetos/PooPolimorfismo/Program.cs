7b3fcac88fd1aa79108d20d9e23986da9969a13e
Lucas Adriano <Lucasadrisilva@hotmail.com>
2021-10-04 10:53:37 -0300

    feat(CSharp): Utilizando polimorfismo

---
+++ Projetos/PooPolimorfismo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado q1 = new Quadrado("Quadrado",2,2);
+            Console.WriteLine(q1.CalcularArea());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projetos/PooPolimorfismo/Quadrado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooPolimorfismo
+{
+    public class Quadrado : Formas
+    {
+        public Quadrado() : base() { }
+        public Quadrado(String Forma, Double Base, Double Altura) : base(Forma, Base, Altura) 
+        { }
+
+        public override Double CalcularArea() 
+        {
+            return this.Base * this.Altura;
+        }
+    }
+}
