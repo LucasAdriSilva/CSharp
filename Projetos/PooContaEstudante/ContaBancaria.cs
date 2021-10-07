a1221c2f3e14ba1f9607bbed27289ffb4eba6e4c
Lucas Adriano <Lucasadrisilva@hotmail.com>
2021-10-07 18:20:52 -0300

    feat(CSharp): Conta bancaria

---
+++ Projetos/PooContaEstudante/ContaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooContaEstudante
+{
+    class ContaBancaria
+    {
+        public Cliente Correntista { get; set; }
+
+        public ContaBancaria()
+        {
+            this.saldo = 0;
+            this.Correntista = new Cliente();
+
+        }
+
+        private Double saldo;
+        public Double Saldo
+        {
+            get { return saldo; }
+
+        }
+
+        public void Depositar(Double valor) { 
+            this.saldo +=
+        }
+        public virtual Double Retirar(Double valor) 
+        {
+            if (this.saldo >= 0)
+            {
+                this.saldo -= valor;
+            }
+            else {
+                throw new Exception("Saldo insuficiente");
+            }
+            return this.saldo;
+        }
+
+    }
+}
---
+++ Projetos/PooContaEstudante/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooContaEstudante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria();
+            conta.Depositar(600);
+            conta.Correntista.Nome = "Lucas Silva";
+
+            try
+            {
+                Console.WriteLine("Valor mpara retirada: ");
+                Double valor = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Saldo restante: " + conta.Retirar(valor));
+            }
+            catch (Exception erro) {
+                Console.WriteLine("Ops, algo deu errado");
+                Console.WriteLine("Error: "+ erro.Message);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
