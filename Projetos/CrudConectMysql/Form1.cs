2a53128b3eb8eb13ff9c2a364e8a5aaaa24ac6a6
Lucas Adriano Silva <lucasadrisilva@hotmail.com>
2021-10-14 19:02:11 -0300

    feat(Crud): Conexão bem sucedida

---
+++ Projetos/CrudConectMysql/Form1.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudConectMysql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Conectar()) 
+            {
+                MessageBox.Show("Conexão bem sucessida!");
+            }
+        }
+
+        private bool Conectar()
+        {
+            var result = false;
+            try
+            {
+                using (var cn = new MySqlConnection(Conn.strConn))
+                {
+                    cn.Open();
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                MessageBox.Show("Falha: " + ex.Message);
+            }
+            return result;
+        }
+    }
+}
