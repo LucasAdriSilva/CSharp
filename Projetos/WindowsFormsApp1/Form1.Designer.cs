a516f8d96a9a9efdc1810cd2c27d5c5c52f1a320
Lucas Adriano Silva <lucasadrisilva@hotmail.com>
2021-10-14 11:14:28 -0300

    feat(Crud):1º Crud C# and MySql

---
+++ Projetos/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvLivros = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLivros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLivros
+            // 
+            this.dgvLivros.AllowUserToAddRows = false;
+            this.dgvLivros.AllowUserToDeleteRows = false;
+            this.dgvLivros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLivros.Location = new System.Drawing.Point(24, 174);
+            this.dgvLivros.Name = "dgvLivros";
+            this.dgvLivros.ReadOnly = true;
+            this.dgvLivros.Size = new System.Drawing.Size(316, 193);
+            this.dgvLivros.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 390);
+            this.Controls.Add(this.dgvLivros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLivros)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvLivros;
+    }
+}
+
---
+++ Projetos/WindowsFormsApp1/LivroBase.cs
@@ -0,0 +1,8 @@
+﻿namespace WindowsFormsApp1
+{
+    internal class LivroBase
+    {
+
+        public static DataTable GetLivros(bool ativos)
+    }
+}
