015a6f1f9b4abe824e198aa9e5d39199e2b105ef
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-05-20 12:58:25 -0300

    feat(C#): Exercicio2

---
+++ Projetos/Curso Novo/01-Execicio/Exer2/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            double x;
+
+            Console.Write("Digite o valor do raio: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            double res = x * x * pi;
+
+            Console.Write("O valor da area do seu circulo é: " + res);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
