992e3459dbc07a51bb06a0334c650bbb88553955
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-05-24 22:25:50 -0300

    Feat(CSharp): Iniciando os estudos novo curso

---
+++ Projetos/Curso Novo/01-Aula/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization; // Para alterar o "." para ","
+
+namespace Primeiro {
+    internal class Program {
+        static void Main(string[] args) {
+            //Matéria ja aprendida -- O que for add aqui é por curiosidade
+            char gen = '\u0041';
+            Console.WriteLine(gen);
+
+
+            double saldo = 10.65865;
+            Console.WriteLine(saldo.ToString("F4")); // O "F" limita a quantidade de caracteres
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine($"{gen} e asdasdasdasda {saldo:F2}");
+
+            /*PAra fazer a conversão é só colocar (tipo de variavel)
+            string saldo = (int)saldo
+            string saldo = (float)saldo
+            int n1 = int.Parse(Console.WriteLine());
+            char c1 = char.Parse(Console.WriteLine());
+            double d1 = double.Parse(Console.WriteLine(), CultureInfo.InvariantCulture);
+            */
+
+            double a = 1.0;
+            double b = -3.0;
+            double c = -4.0;
+
+            double res = (double)10.0 / 8;
+            Console.WriteLine(res);
+            Console.WriteLine("-----------------------------------------------");
+
+            /*formulas mátematicas Bascara*/
+
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c; //Exponenciação
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a); //Raiz quadrada
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a); //Raiz quadrada
+
+            Console.WriteLine($"{x1} e {x2}");
+
+
+
+            /*Comando Split*/
+            string s = Console.ReadLine();
+            string[] vet = s.Split(' ');
+
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine($"Testando {p1} uhasauhsuhas {p2} asuhausuahsuha {p3}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projetos/Curso Novo/01-Execicio/Exer3/Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] table = new string[4, 2];
+
+            table[0, 0] = "1"; table[1, 0] = "2"; table[2, 0] = "3"; table[3, 0] = "4";
+            table[4, 0] = "5";
+
+            table[0, 1] = "Cachorro Quente"; table[1, 1] = "X-Salada";table[2, 1] = "X-Bacon";
+            table[3, 1] = "Torrada Simples";table[4, 1] = "Refrigerante";
+
+            table[0, 2] = "4,00";table[1, 2] = "4,50";table[2, 2] = "5,00"; table[3, 2] = "2,00";
+            table[4, 2] = "1,50";
+
+            
+
+            Console.WriteLine(table);
+
+            Console.WriteLine("Escolah seu pedido: ");
+            double var1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Deseja mais alguma coisa? Se sim escolha o código:  ");
+            double var2 = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            //double res { }
+
+            //Console.WriteLine("A conta deu: "+ res)
+        }
+    }
+}
