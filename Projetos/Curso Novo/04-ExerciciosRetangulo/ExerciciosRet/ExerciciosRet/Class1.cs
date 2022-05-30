8e2b8604072adf544e70d7f51a1986fef316813a
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-05-30 20:08:09 -0300

    feat(Csharp): Area perimetroe diagonal de um retangulo

---
+++ Projetos/Curso Novo/04-ExerciciosRetangulo/ExerciciosRet/ExerciciosRet/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosRet {
+    internal class Retangulo {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public double AreaRet() {
+            return Base * Altura;
+        }
+
+        public double PerimetroRet() {
+            return (2* Base) + (2* Altura);
+        }
+
+        public double DiagonaloRet() {
+            return Math.Sqrt((Altura * Altura) + (Base * Base));
+        }
+
+    }
+}
---
+++ Projetos/Curso Novo/04-ExerciciosRetangulo/ExerciciosRet/ExerciciosRet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosRet {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Retangulo r = new Retangulo();  
+
+            Console.WriteLine("Digite o valores do retangulo");
+
+            Console.Write("Altura: ");
+            r.Altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Largura: ");
+            r.Base = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"De acordo com os dados, sua área é {r.AreaRet()}cm  e seu perimetro é {r.PerimetroRet()}cm² com uma diagonal de {r.DiagonaloRet()}cm");
+
+        }
+    }
+}
