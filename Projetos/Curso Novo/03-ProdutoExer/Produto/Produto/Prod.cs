3c98570c2bf36ea584877bc6e0c9b26062343039
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-05-30 19:42:48 -0300

    feat(Csharp): usando ToString na classe base

---
+++ Projetos/Curso Novo/03-ProdutoExer/Produto/Produto/Prod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Produto {
+    internal class Prod {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public double ValorTotalemEstoque() {
+            return Price * Quantity;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+            Quantity += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) {
+            Quantity -= quantidade;
+        }
+
+        public override string ToString() {
+            return Name + ", $" + Price.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantity + " unidades, Total: $" + ValorTotalemEstoque();
+        }
+    }
+}
---
+++ Projetos/Curso Novo/03-ProdutoExer/Produto/Produto/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Produto {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Prod p = new Prod();
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");  
+            p.Name = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            p.Quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto " + p);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o numero que você quer add ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine("Dados atualizados "+ p);
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Digite o numero que você quer remover ao estoque: ");
+            int qte2 = int.Parse(Console.ReadLine());
+
+            p.RemoverProdutos(qte2);
+
+            Console.WriteLine("Dados atualizados " + p);
+        }
+    }
+}
