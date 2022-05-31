53bbc29c966e504b2eeab623a2cd86c3c20079ec
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-05-30 21:40:54 -0300

    feat(Csharp): Usando static para fazer um conversor

---
+++ Projetos/Curso Novo/06-Conversor/Conversor/Conversor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Conversor {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Qual é a cotação do dóler? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quanto você quer comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double res = ConversorDeMoeda.Conversor(quantia, cotacao);
+
+            Console.WriteLine($"O valor é : {res}");
+        }
+    }
+}
