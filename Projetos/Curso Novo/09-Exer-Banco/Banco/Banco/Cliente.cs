4a07ef058806e9bef899e31ca16a9dee20475b40
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-06-02 16:46:36 -0300

    feat(Csharp): Usando OO para um banco

---
+++ Projetos/Curso Novo/09-Exer-Banco/Banco/Banco/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco {
+    class Cliente {
+
+        public string NomeTitular { get; set; }
+        public int NumConta { get; set; }
+        public double Saldo { get; private set; }
+
+        public double Depesito(double valor) {
+            Saldo += valor;
+            return Saldo;
+        }
+
+        public double Saque(double valor) {
+            Saldo -= valor;
+            return Saldo;
+        }
+
+        public override string ToString() {
+            return $"Conta {NumConta}, Titular: {NomeTitular}, Saldo ${Saldo.ToString()}";
+            //return "Conta "+ NumConta+ ", Titular: "+ NomeTitular+ ", Saldo: $ "+ Saldo.ToString();
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Projetos/Curso Novo/09-Exer-Banco/Banco/Banco/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Banco {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Cliente c = new Cliente();
+            char resp;
+
+            Console.Write("Entre com o número da conta: ");
+            c.NumConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            c.NomeTitular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n) ?");
+            resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S') {
+                Console.Write("");
+                Console.Write("Entre o valor de depósito inicial: ");
+                double valorDep = double.Parse(Console.ReadLine());
+                c.Depesito(valorDep);
+                Console.WriteLine(c.ToString());
+            }
+            else {
+                Console.Write("Dados da conta: ");
+                Console.WriteLine(c.ToString());
+
+            }
+
+
+            Console.Write("Entre um valor para depósito: ");
+            double d = double.Parse(Console.ReadLine());
+            c.Depesito(d);
+            Console.WriteLine(c.ToString());
+
+
+            Console.Write("Entre um valor para saque: ");
+            double s = double.Parse(Console.ReadLine());
+            c.Saque(s);
+            Console.WriteLine(c.ToString());
+
+
+
+
+
+
+
+
+        }
+    }
+}
