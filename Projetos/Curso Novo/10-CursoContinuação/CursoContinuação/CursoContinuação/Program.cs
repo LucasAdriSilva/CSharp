00c19450ac43f3557024a5ef23aa913ce92cad90
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-06-03 07:38:43 -0300

    feat(Csharp): Funtion nativas e trabalhando com null

---
+++ Projetos/Curso Novo/10-CursoContinuação/CursoContinuação/CursoContinuação/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoContinuação {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //double x = null;
+            //Nullable<double> x = null;
+            double? x = null;
+
+            double? y = x ?? 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue) {
+                Console.WriteLine(x.Value);
+            }
+            else {
+                Console.WriteLine("x is Null");
+            }
+
+            if (y.HasValue) {
+                Console.WriteLine(y.Value);
+            }
+            else {
+                Console.WriteLine("y is Null");
+            }
+            
+
+        }
+    }
+}
