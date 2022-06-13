c1a83c29d161c8adc9082e80c4c746ebdb58d11b
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-06-13 19:10:38 -0300

    feat(Csharp): usando params para funções com parametros ilimitados

---
+++ Projetos/Curso Novo/12-Modificador de param/ModificadorDeParam/ModificadorDeParam/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadorDeParam {
+    internal class Program {
+        static void Main(string[] args) {
+            int s1 = Calculator.Sum(2, 5);
+            Console.WriteLine(s1);
+
+            int s2 = Calculator.Sum(2, 5,1);
+            Console.WriteLine(s2);
+
+            int s3 = Calculator.Sum(2, 5,1);
+            Console.WriteLine(s3);
+
+            int s4 = Calculator.Summ(2,3,6,5,8,9,1,2,3,3,3);
+
+            Console.WriteLine(s4);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projetos/Curso Novo/12-Modificador de param/ModificadorDeParam/ModificadorDeParam/calculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadorDeParam {
+    class Calculator {
+        public static int Sum(int n1, int n2) {
+            return n1 + n2;
+        }
+        public static int Sum(int n1, int n2, int n3) {
+            return n1 + n2 + n3;
+        }
+        public static int Sum(int n1, int n2, int n3, int n4) {
+            return n1 + n2 + n3 + n4;
+        }
+
+        public static int Summ(params int[] numbers ) { 
+            int sub = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sub += numbers[i];
+            }
+            return sub; 
+        }
+    }
+}
