80e23e28f4416772a067ae035f33ac091ac9a80f
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-06-13 19:36:24 -0300

    feat(Csharp): Usando foreach

---
+++ Projetos/Curso Novo/13-Foreach/Foreach/Foreach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach {
+    internal class Program {
+        static void Main(string[] args) {
+
+            string[] vect = new string[] { "Carlos", "Joao", "Maria" };
+
+            for (int i = 0; i < vect.Length; i++) {
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine("-----------------");
+
+            foreach (string var in vect)
+                Console.WriteLine(var);
+        }
+    }
+}
+    
+
