f8925d6cb6c13cd61147910701071da8c914bc8b
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-06-16 14:09:20 -0300

    feat(Csharp): Manipulando arquivos de texto

---
+++ Projetos/Curso Novo/15-ManipulandoTXT/ManipulandoTXT/ManipulandoTXT/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulandoTXT {
+    internal class Program {
+        static void Main(string[] args) {
+            string x = "Hello2";
+            string line;
+
+            //*************************** Trecho para criar
+            try {
+                //Open the File
+                StreamWriter file = new StreamWriter("D:\\Test1.txt", false);
+                file.WriteLine(x);
+                file.WriteLine("Teste");
+                file.WriteLine("Teste");
+
+                file.Close();
+            }
+            catch (Exception e) {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally {
+                Console.WriteLine("Finalizado");
+            }
+
+
+            // *************************** Trecho apra ler
+            try {
+                //Cria ou abre arquivo já criado
+                StreamReader sr = new StreamReader("D:\\Test1.txt");
+
+                //Lê a priemira linha
+                line = sr.ReadLine();
+
+                //Lógica para escrever a linah e depois ler a proxima linha
+                while (line != null) {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e) {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
