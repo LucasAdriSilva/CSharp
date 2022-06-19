d40b696d6f7eb19e851717295dae96d22c7484a9
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-06-19 18:57:02 -0300

    feat(Csharp): Mais funções de manipulação de DataTime

--- Projetos/Curso Novo/20-DateTme/DataTime/DataTime/Program.cs
+++ Projetos/Curso Novo/20-DateTme/DataTime/DataTime/Program.cs
@@ -48,6 +48,50 @@ namespace DataTime {
             DateTime FR4 = DateTime.ParseExact("2022-12-15 13:05:56", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
             Console.WriteLine(FR4);
 
+            Console.ReadKey();
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+
+            DateTime d = new DateTime(2011,8,15,13,45,58);
+            Console.WriteLine(d);
+            Console.WriteLine("1) Data: "+ d.Date);
+            Console.WriteLine("2) Data: "+ d.Day);
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year);
+
+
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.ReadKey();
+
+            //Função para escrevcer a data por extenso DIA DA SEMANA, NUMERICO DO MES TAL DE TAL ANO
+            string s1 = d.ToLongDateString();
+            Console.WriteLine(s1);
+
+            string s2 = d.ToLongTimeString();
+            Console.WriteLine(s2);
+
+            string s3 = d.ToShortDateString();
+            Console.WriteLine(s3);
+
+            string s4 = d.ToShortTimeString();
+            Console.WriteLine(s4);
+
+            string s5 = d.ToString();
+            Console.WriteLine(s5);
+
+            string s6 = d.ToString("yyyy-MM-dd HH:mm:ss");
+            Console.WriteLine(s6);
+
+            string s7 = d.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            Console.WriteLine(s7);
 
             Console.ReadKey();
         }
