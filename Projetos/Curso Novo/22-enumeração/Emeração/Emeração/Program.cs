1cae7763ccf8adb0ba83ca3fcfc93b37fb0fa615
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-06-21 22:10:56 -0300

    feat(Csharp): Priemira ultilização de Enum

---
+++ Projetos/Curso Novo/22-enumeração/Emeração/Emeração/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Emeração.Entitties.Enuns;
+using Emeração.Entitties;
+
+
+namespace Emeração
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order {
+            Id = 1080,
+            Moment = DateTime.Now,
+            Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+            Console.ReadKey();
+            Console.Clear();
+
+            //Convertando enum para string
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+            Console.ReadKey();
+            Console.Clear();
+
+            //Convertendo enum para string
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+            Console.WriteLine(os);
+
+
+        }
+    }
+}
