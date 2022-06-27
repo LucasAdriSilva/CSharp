9b9aee9a57cbacd1eed5f5428abf285b78f4cac5
lucasadrisilva <lucasadrisilva@hotmail.com>
2022-06-27 20:30:31 -0300

    feat(Csharp): Iniciando um projeto

---
+++ Projetos/Curso Novo/23-Exercicio(Woker-Trabalhador)/StringBuiler/StringBuiler/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace StringBuiler.Entities
+{
+    internal class Comment
+    {
+
+        public string Text { get; set; }
+
+        public Comment() { }
+
+        public Comment(string text)
+        {
+            text = Text;
+        }
+    }
+}
---
+++ Projetos/Curso Novo/23-Exercicio(Woker-Trabalhador)/StringBuiler/StringBuiler/Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuiler.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+    }
+}
